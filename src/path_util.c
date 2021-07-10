e818792dd3230c99d6d069b3f211e6ae54adc7c6
Mateusz Okulus <mmokulus@gmail.com>
2021-07-10 13:51:35 +0200

    Remove join_path

--- src/path_util.c
+++ src/path_util.c
@@ -3,19 +3,6 @@
 #include <string.h>
 #include <wordexp.h>
 
-char *join_path(const char *a, const char *b)
-{
-	const size_t len_a = strlen(a);
-	const size_t len_b = strlen(b);
-	const size_t total_len = len_a + 1 + len_b + 1;
-	char *path = malloc(total_len);
-	memcpy(path, a, len_a);
-	path[len_a] = '/';
-	memcpy(path + len_a + 1, b, len_b);
-	path[len_a + 1 + len_b] = '\0';
-	return path;
-}
-
 char *expand_path(const char *path)
 {
 	wordexp_t exp;
--- src/path_util.h
+++ src/path_util.h
@@ -1,4 +1,3 @@
 #pragma once
 
 char *expand_path(const char *path);
-char *join_path(const char *a, const char *b);
